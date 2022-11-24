bd4dc800a51136af11ee8149d3b51ba03884ced9
Eric Krebs <ekrebs@outlook.com>
2022-11-23 23:34:50 -0600

    formating changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import AdventureEditor from '../components/AdventureEditor';
 
-//hello
 export default function HomePage() {
-
 	return <div>
 		<AdventureEditor/>
 	</div>;
--- components/AdventureEditor.tsx
+++ components/AdventureEditor.tsx
@@ -1,6 +1,8 @@
 'use client';
 
+import { useState, } from 'react';
+import Link from 'next/link';
+
 export default function AdventureEditor() {
 	return <>Editor</>;
-
 }
